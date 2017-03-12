829acc0b103ae1d1c7e4aac19d8d5d64da14ea23
konojunya <junya03dance@gmail.com>
2017-03-12 14:10:41 +0900

    自動演奏実装

--- grove-buzzer/js/script.js
+++ grove-buzzer/js/script.js
@@ -1,3 +1,32 @@
+var virtual_keyboard = {
+  1: "523.251",
+  2: "554.365",
+  3: "587.330",
+  4: "622.254",
+  5: "659.255",
+  6: "698.456",
+  7: "739.989",
+  8: "783.991",
+  9: "830.609",
+  a: "880.000",
+  b: "932.328",
+  c: "987.767",
+  d: "1046.502"
+};
+
+$(".play").on("click",function(){
+  var input = $(".auto-play-input").val()
+  var delay = 600
+  for(var i = 0; i < input.length; i++){
+    var char = input[i];
+    var scale = virtual_keyboard[char]
+
+    setTimeout(function(){
+      playTheSounds(scale)
+    }, i * delay)
+  }
+});
+
 $(".js-keyboard").on("click",function(e){
   var scale = changeScale(e.target.dataset.vs);
   playTheSounds(scale);
