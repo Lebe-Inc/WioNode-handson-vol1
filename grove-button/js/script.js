d00c1cec4e7eef762b83f08ba65c3967e1fabd01
konojunya <junya03dance@gmail.com>
2017-03-12 13:13:53 +0900

    :+1: ブザーを使ったピアノ

--- grove-button/js/script.js
+++ grove-button/js/script.js
@@ -7,13 +7,6 @@ ws.onopen = function() {
     ws.send("1d1b1571e08c528ad954b42f951cdc52");
 };
 ws.onmessage = function (e) {
-  var json = JSON.parse(e.data);
-  var num = json.msg.button_pressed;
-  if(num == 5) isOn = !isOn;
-
-  if(isOn){
-    led.src = "img/led-on.png";
-  }else{
-    led.src = "img/led-off.png";
-  }
+  isOn = !isOn;
+  led.src = isOn ? "img/led-on.png" : "img/led-off.png";
 };
